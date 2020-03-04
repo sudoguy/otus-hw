3f8c38b9717b8a9f0c320acb30736f1b2732e0b4
Evgeny Kemerov <eskemerov@gmail.com>
2020-03-04 15:23:35 +0300

    Fix linter issues

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
-	"github.com/beevik/ntp"
 	"log"
 	"os"
 	"time"
-)
 
+	"github.com/beevik/ntp"
+)
 
 func main() {
 	logger := log.New(os.Stdout, "", log.Lmsgprefix)
+
+	exitCode := 0
 
 	currentTime := time.Now()
 	ntpTime, err := ntp.Time("pool.ntp.org")
 
 	if err != nil {
 		log.Println(err)
-		defer os.Exit(1)
+		exitCode = 1
 	}
 
 	logger.Println("current time:", currentTime)
 	logger.Println("exact time:", ntpTime)
+
+	os.Exit(exitCode)
 }
