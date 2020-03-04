ff9bc56b55348eb9589881064920ec23cd98fcf0
Evgeny Kemerov <eskemerov@gmail.com>
2020-03-04 14:49:45 +0300

    Add hw01_hello_now

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"github.com/beevik/ntp"
+	"log"
+	"os"
+	"time"
+)
+
+
 func main() {
-	// Place your code here
+	logger := log.New(os.Stdout, "", log.Lmsgprefix)
+
+	currentTime := time.Now()
+	ntpTime, err := ntp.Time("pool.ntp.org")
+
+	if err != nil {
+		log.Println(err)
+		defer os.Exit(1)
+	}
+
+	logger.Println("current time:", currentTime)
+	logger.Println("exact time:", ntpTime)
 }
